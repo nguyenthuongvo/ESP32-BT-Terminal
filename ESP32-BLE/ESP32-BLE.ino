9d73f7be6d57ba5e57e7ae2206051f7733488f3e
Vo Thuong Nguyen <49666553+nguyenthuongvo@users.noreply.github.com>
2021-07-17 16:39:39 +0700

    ESP32 advertise after disconnect

--- ESP32-BLE/ESP32-BLE.ino
+++ ESP32-BLE/ESP32-BLE.ino
@@ -15,12 +15,12 @@
 
  
 #define BLE_NAME "ESP32" //must match filters name in bluetoothterminal.js- navigator.bluetooth.requestDevice
-//#define SERVICE_UUID        "6e400001-b5a3-f393-e0a9-e50e24dcca9e" //- must match optional services on navigator.bluetooth.requestDevice
-//#define CHARACTERISTIC_UUID "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+#define SERVICE_UUID        "6e400001-b5a3-f393-e0a9-e50e24dcca9e" //- must match optional services on navigator.bluetooth.requestDevice
+#define CHARACTERISTIC_UUID "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
 //maintain compatability with HM-10
-BLEUUID  SERVICE_UUID((uint16_t)0xFFE0); // UART service UUID
-BLEUUID CHARACTERISTIC_UUID ((uint16_t)0xFFE1);
+//BLEUUID  SERVICE_UUID((uint16_t)0xFFE0); // UART service UUID
+//BLEUUID CHARACTERISTIC_UUID ((uint16_t)0xFFE1);
 
 /*
  * navigator.bluetooth.requestDevice({
@@ -34,7 +34,15 @@ BLEUUID CHARACTERISTIC_UUID ((uint16_t)0xFFE1);
     }).
 */
 
+class MyServerCallbacks: public BLEServerCallbacks {
 
+  void onDisconnect(BLEServer* pServer) {
+    Serial.println("Disconnect");
+    delay(2000);
+    pServer->getAdvertising()->start();
+  }
+  
+};
 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pCharacteristic) {
@@ -58,6 +66,7 @@ class MyCallbacks: public BLECharacteristicCallbacks {
     }
 };
 
+
 void setup() {
   Serial.begin(115200);
 
@@ -69,9 +78,10 @@ void setup() {
 
   BLEDevice::init(BLE_NAME);
   BLEServer *pServer = BLEDevice::createServer();
-
+  pServer -> setCallbacks(new MyServerCallbacks());
+  
   BLEService *pService = pServer->createService(SERVICE_UUID);
-
+  
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
@@ -86,6 +96,7 @@ void setup() {
   pService->start();
 
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  
   pAdvertising->start();
 }
 
